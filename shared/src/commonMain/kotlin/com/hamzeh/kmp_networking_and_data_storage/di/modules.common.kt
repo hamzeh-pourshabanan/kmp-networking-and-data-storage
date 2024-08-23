45b4985f773567548ebc5ccebe17e7bcb87a57ad
hamzeh-pourshabanan <hamzeh.p89@gmail.com>
2024-08-23 23:13:43 +0330

    Provide remote datasource

--- shared/src/commonMain/kotlin/com/hamzeh/kmp_networking_and_data_storage/di/modules.common.kt
+++ shared/src/commonMain/kotlin/com/hamzeh/kmp_networking_and_data_storage/di/modules.common.kt
@@ -1,6 +1,8 @@
 package com.hamzeh.kmp_networking_and_data_storage.di
 
 import com.hamzeh.kmp_networking_and_data_storage.data.network.apiClient
+import com.hamzeh.kmp_networking_and_data_storage.data.sources.SpaceXRemoteDataSource
+import com.hamzeh.kmp_networking_and_data_storage.data.sources.ktor.KtorSpaceXRemoteDataSource
 import io.ktor.client.HttpClient
 import org.koin.dsl.module
 
@@ -8,4 +10,5 @@ private const val BASE_URL = "https://api.spacexdata.com"
 
 val coreModule = module {
     single<HttpClient> { apiClient(BASE_URL) }
+    single<SpaceXRemoteDataSource> { KtorSpaceXRemoteDataSource(get<HttpClient>()) }
 }
