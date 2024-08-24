78e54dd9c5a9d6fa5cac2c182300270f6f4580ec
hamzeh-pourshabanan <hamzeh.p89@gmail.com>
2024-08-24 20:57:58 +0330

    Update local data source

--- shared/src/commonMain/kotlin/com/hamzeh/kmp_networking_and_data_storage/data/db/entity/RocketLaunchEntity.kt
+++ shared/src/commonMain/kotlin/com/hamzeh/kmp_networking_and_data_storage/data/db/entity/RocketLaunchEntity.kt
@@ -5,7 +5,8 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "rocket_launches")
 data class RocketLaunchEntity(
-    @PrimaryKey val flightNumber: Int,
+    @PrimaryKey val id: String,
+    val flightNumber: Int,
     val missionName: String,
     val launchDate: String,
     val details: String?,
