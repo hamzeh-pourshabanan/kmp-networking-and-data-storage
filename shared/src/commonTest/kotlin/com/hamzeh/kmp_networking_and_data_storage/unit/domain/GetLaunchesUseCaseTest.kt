b5dbeb0e5ea607350b1cf44dd16291d90c8e8878
hamzeh-pourshabanan <hamzeh.p89@gmail.com>
2024-08-24 21:02:01 +0330

    Write usecase test

--- shared/src/commonTest/kotlin/com/hamzeh/kmp_networking_and_data_storage/unit/domain/GetLaunchesUseCaseTest.kt
+++ shared/src/commonTest/kotlin/com/hamzeh/kmp_networking_and_data_storage/unit/domain/GetLaunchesUseCaseTest.kt
@@ -3,7 +3,7 @@ package com.hamzeh.kmp_networking_and_data_storage.unit.domain
 import app.cash.turbine.test
 import com.hamzeh.kmp_networking_and_data_storage.data.repository.DefaultRocketLaunchRepository
 import com.hamzeh.kmp_networking_and_data_storage.domain.usecase.getLaunches.DefaultGetLaunchesUseCase
-import com.hamzeh.kmp_networking_and_data_storage.domain.usecase.refreshLaunches.RefreshRocketLaunches
+import com.hamzeh.kmp_networking_and_data_storage.domain.usecase.refreshLaunches.DefaultRefreshRocketLaunchesUseCase
 import com.hamzeh.kmp_networking_and_data_storage.unit.data.resources.FakeSpaceXRemoteDataSource
 import com.hamzeh.kmp_networking_and_data_storage.unit.data.resources.InMemorySpaceXLocalDataSource
 import com.hamzeh.kmp_networking_and_data_storage.unit.data.resources.StupSpaceXRemoteDataSource
@@ -16,7 +16,7 @@ class GetLaunchesUseCaseTest {
     private val localDataSource = InMemorySpaceXLocalDataSource()
     private val repository = DefaultRocketLaunchRepository(localDataSource, remoteDataSource)
     private val getLaunchesUseCase = DefaultGetLaunchesUseCase(repository)
-    private val refreshUseCase = RefreshRocketLaunches(repository)
+    private val refreshUseCase = DefaultRefreshRocketLaunchesUseCase(repository)
 
     @Test
     fun `when RefreshUseCased has not been called then GetLaunchesUseCase returns empty`() = runTest{
@@ -38,7 +38,7 @@ class GetLaunchesUseCaseTest {
         val remote = StupSpaceXRemoteDataSource("missingFieldsLaunches.json")
         val repository = DefaultRocketLaunchRepository(localDataSource, remote)
         val getLaunchesUseCase = DefaultGetLaunchesUseCase(repository)
-        val refreshUseCase = RefreshRocketLaunches(repository)
+        val refreshUseCase = DefaultRefreshRocketLaunchesUseCase(repository)
         refreshUseCase.invoke()
         getLaunchesUseCase.invoke().test {
             assertEquals(true, awaitItem().isEmpty())
